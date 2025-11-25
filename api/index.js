0dfbe387146e4c109c6c0354862e0766d1ece2d4
Tapiwanashe Marufu <t.marufu@alustudent.com>
2025-11-25 15:03:16 +0200

    added serverless

---
+++ api/index.js
@@ -0,0 +1,41 @@
+import serverless from 'serverless-http'
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from '../config/db.js'
+import clerkWebhooks from '../controllers/webhooks.js'
+import '../config/instrument.js'
+import * as Sentry from '@sentry/node'
+
+const app = express()
+
+// Middlewares
+app.use(cors())
+app.use(express.json())
+
+// Connect to DB per request (avoids top-level await issues)
+let dbConnected = false
+app.use(async (req, res, next) => {
+  if (!dbConnected) {
+    try {
+      await connectDB()
+      dbConnected = true
+    } catch (err) {
+      return next(err)
+    }
+  }
+  next()
+})
+
+// Routes
+app.get('/', (req, res) => res.send('API Working'))
+app.get('/debug=sentry', (req, res) => {
+  throw new Error('My first Sentry error!')
+})
+app.post('/webhooks', clerkWebhooks)
+
+// Sentry error handler
+Sentry.setupExpressErrorHandler(app)
+
+// Export handler for Vercel
+export const handler = serverless(app)
--- server/server.js
+++ server/server.js
@@ -4,31 +4,31 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/db.js'
 import * as Sentry from "@sentry/node"
-import clerkWebhooks from "./controllers/webhooks.js";
+import clerkWebhooks from "./controllers/webhooks.js"
 
-// Initlialize Express
-
+// Initialize Express
 const app = express()
 
-//Connet to database
+// Connect to database
 await connectDB()
 
-// Middlwares
+// Middlewares
 app.use(cors())
 app.use(express.json())
 
-//Routes
-app.get('/',(req,res)=>res.send("API Working"))
-app.get("/debug=sentry", function mainHandler(req, res){
-    throw new Error("My first Sentry error!") 
+// Routes
+app.get('/', (req, res) => res.send("API Working"))
+app.get("/debug=sentry", (req, res) => {
+    throw new Error("My first Sentry error!")
 })
-app.post('/webhooks',clerkWebhooks)
+app.post('/webhooks', clerkWebhooks)
 
 // Port
 const PORT = process.env.PORT || 5000
 
-Sentry.setupExpressErrorHandler(app);
+Sentry.setupExpressErrorHandler(app)
 
-app.listen(PORT,()=>{
-    console.log(`Server in running on port ${PORT}`)
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
 })
